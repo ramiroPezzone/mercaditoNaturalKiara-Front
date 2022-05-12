3161756fd0184476c2395697e2d39c6521be42be
ramiroPezzone <ramiro.pezzone@gmail.com>
2022-05-12 15:45:33 -0300

    corrección

--- src/pages/js/ProductosUsuarios.jsx
+++ src/pages/js/ProductosUsuarios.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 import axios from "axios";
 import styles from '../css/ProductosUsuarios.module.css'
 import stylesCarrito from "../../components/css/IconoCarrito.module.css";
