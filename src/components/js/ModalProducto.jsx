e4cd02c2087d05a6d2d1fce8b870646050e1029f
ramiroPezzone <ramiro.pezzone@gmail.com>
2022-05-15 12:15:47 -0300

    agregado de scroll a la descripción de los modals

--- src/components/js/ModalProducto.jsx
+++ src/components/js/ModalProducto.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import styles from '../css/ModalProducto.module.css'
 
 const ModalProducto = ({ detalles, ocultarModal }) => {
 
+  const detallesModal = useRef()
+
   const cerrarModal = () => {
     ocultarModal()
   }
+
+  useEffect(() => {
+    if (detalles.id !== undefined) {
+      document.querySelector("*").style.overflowY = "hidden"
+      console.log(detallesModal.current);
+      detallesModal.current.style.overflowY = "scroll"
+      // document.querySelector(".descriptionModal").style.overflowY = "hidden"
+    } else {
+      document.querySelector("*").style.overflowY = "initial"
+    }
+  }, [detalles.id])
 
   return (
     <div
@@ -21,7 +34,7 @@ const ModalProducto = ({ detalles, ocultarModal }) => {
         <div className={styles.detallesProducto}>
           <div className={styles.containerImg} style={{ backgroundImage: `url(${detalles.image})` }} />
           <div className={styles.textoProducto}>
-            <p className={styles.descriptionModal}>{detalles.description}</p>
+            <p className={styles.descriptionModal} ref={detallesModal}>{detalles.description}</p>
             <p className={styles.unityModal}>Venta por {detalles.unity}</p>
             <p className={styles.priceModal}>${detalles.price}</p>
           </div>
